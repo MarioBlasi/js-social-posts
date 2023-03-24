528078cf1d5c4295412f6e6961fdffa12a7afe18
MarioBlasi <mblasi509@gmail.com>
2023-03-24 14:28:48 +0000

    array

--- main.js
+++ main.js
@@ -1,57 +1,48 @@
 const posts = [
-    {
-        "id": 1,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/300?image=171",
-        "author": {
-            "name": "Phil Mangione",
-            "image": "https://unsplash.it/300/300?image=15"
-        },
-        "likes": 80,
-        "created": "2021-06-25"
-    },
-    {
-        "id": 2,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=112",
-        "author": {
-            "name": "Sofia Perlari",
-            "image": "https://unsplash.it/300/300?image=10"
-        },
-        "likes": 120,
-        "created": "2021-09-03"
-    },
-    {
-        "id": 3,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=234",
-        "author": {
-            "name": "Chiara Passaro",
-            "image": "https://unsplash.it/300/300?image=20"
-        },
-        "likes": 78,
-        "created": "2021-05-15"
-    },
-    {
-        "id": 4,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=24",
-        "author": {
-            "name": "Luca Formicola",
-            "image": null
-        },
-        "likes": 56,
-        "created": "2021-04-03"
-    },
-    {
-        "id": 5,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=534",
-        "author": {
-            "name": "Alessandro Sainato",
-            "image": "https://unsplash.it/300/300?image=29"
-        },
-        "likes": 95,
-        "created": "2021-03-05"
-    }
+  {
+    id: 1,
+    authorName: "Phil Mangione",
+    authorPhoto: "https://unsplash.it/300/300?image=15",
+    date: "03-22-2023",
+    text: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+    image: "https://unsplash.it/600/300?image=171",
+    likes: 80,
+  },
+  {
+    id: 2,
+    authorName: "John Doe",
+    authorPhoto: "https://unsplash.it/300/300?image=22",
+    date: "03-20-2023",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius quis elit non volutpat. Sed gravida nunc at nunc posuere, sed aliquam sapien hendrerit.",
+    likes: 42,
+  },
+  {
+    id: 3,
+    authorName: "Jane Smith",
+    authorPhoto: "https://unsplash.it/300/300?image=31",
+    date: "03-18-2023",
+    text: "Donec sit amet justo vel risus dignissim molestie nec sit amet arcu. In hac habitasse platea dictumst. Ut sagittis massa vel iaculis suscipit.",
+    image: "https://unsplash.it/600/300?image=310",
+    likes: 25,
+  },
 ];
+
+const rowEl = document.querySelector(".row");
+console.log(posts);
+
+posts
+  .map((thisposts) => {
+    return `
+      <div class="col">
+        <div class="card">
+          <img src="${thisposts.image}" alt="" class="card-img-top">
+          <div class="card-body">
+            <h3>${thisposts.title}</h3>
+            <p>${thisposts.text}</p>
+          </div>
+        </div>
+      </div>`;
+  })
+  .forEach((markup) => {
+    rowEl.innerHTML += markup;
+  });
